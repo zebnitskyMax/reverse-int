9952bc3f492193a96fe8a917da98dca6d7255d49
zebnitskyMax <58050667+zebnitskyMax@users.noreply.github.com>
2020-03-21 22:18:39 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n * (-1);
+        return Number.parseInt(n.toString().split('').reverse().join(''));
+    } else {
+        return Number.parseInt(n.toString().split('').reverse().join(''));
+    }
 }
